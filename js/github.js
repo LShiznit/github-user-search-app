7665e037f9a71f8b45c1c7f859a0ede1d09a48f6
Leo <lmaximilians@gmail.com>
2022-05-25 21:22:48 +0200

    remove keys

--- js/github.js
+++ js/github.js
@@ -1,15 +1,11 @@
 class Github {
   constructor() {
-    this.client_id = '1d175955353c8bc2cd9b'
-    this.client_secret = '82ddb728def81e7eb3333da51db8f9a328786c45'
-    this.repos_count = 5
-    this.repos_sort = 'created: asc'
+    // this.client_id =
+    // this.client_secret =
   }
 
   async getUser(user) {
-    const userResponse = await fetch(
-      `https://api.github.com/users/${user}?&client_id=${this.client_id}&client_secret=${this.client_secret}`
-    )
+    const userResponse = await fetch(`https://api.github.com/users/${user}?`)
     const userProfile = await userResponse.json()
 
     return {
