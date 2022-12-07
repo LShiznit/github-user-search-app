b1f4ed97ed3db8ad62230e892cd0f7b5473c3ee1
Leopoldini <lmaximilians@gmail.com>
2022-12-07 10:34:44 +0100

    filled avatar image

--- js/ui.js
+++ js/ui.js
@@ -1,16 +1,16 @@
 class UI {
   constructor() {
-    this.profile = document.getElementById('profile')
+    this.profile = document.getElementById("profile")
   }
 
   // Display profile in UI
   showProfile(user) {
     console.log(user)
 
-    this.profile.innerHTML = ''
+    this.profile.innerHTML = ""
     this.profile.innerHTML = `
       <div class="grid-container card">
-        <div class="profile-image"></div>
+        <div class="profile-image" id="profile-image"></div>
         <div class="profile-name">
           <h1>${user.name}</h1>
           <a href="${user.html_url}"><h3>@${user.login}</h3></a>
@@ -51,24 +51,28 @@ class UI {
       </div>
     </div>
     `
+    // fill image avatar
+    document.getElementById(
+      "profile-image"
+    ).style.backgroundImage = `url(${user.avatar_url})`
 
     function giveJoinDate(e) {
-      const joinDate = e.split('T')[0].split('-')
+      const joinDate = e.split("T")[0].split("-")
       const months = [
-        'Jan',
-        'Feb',
-        'Mar',
-        'Apr',
-        'May',
-        'Jun',
-        'Jul',
-        'Aug',
-        'Sept',
-        'Oct',
-        'Nov',
-        'Dec',
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sept",
+        "Oct",
+        "Nov",
+        "Dec",
       ]
-      return joinDate[2] + ' ' + months[joinDate[1] - 1] + ' ' + joinDate[0]
+      return joinDate[2] + " " + months[joinDate[1] - 1] + " " + joinDate[0]
     }
   }
 }
